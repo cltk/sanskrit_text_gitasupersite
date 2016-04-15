f5e089279a76edd71aef64aca2921628348ab48a
chaitanyasai.alaprthi <achaitanyasai@gmail.com>
2016-04-15 19:10:29 +0530

    Added minigita and updated README

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Sanskrit texts from [Gita Supersite corpus](http://www.gitasupersite.iitk.ac.in/
  - <a href="http://www.gitasupersite.iitk.ac.in/srimad?language=dv&field_chapter_value=1&field_nsutra_value=1">Srimad Bhagavadgita</a>
  - <a href="http://www.valmiki.iitk.ac.in/">Valmiki Ramayana</a>
  - <a href="http://www.gitasupersite.iitk.ac.in/bs_home">Brahma Sutra</a>
+ - <a href="http://www.gitasupersite.iitk.ac.in/minigita/texts">Mini Gita</a>
 
 These files are in the public domain and generously made publicly available by the Gita Supersite. See `LICENSE.md` for more.
---
+++ minigita/scrape.py
@@ -0,0 +1,86 @@
+import urllib
+import BeautifulSoup
+import os
+import sys
+
+category = ''
+
+def Parse(val0, val1):
+   Url = 'http://www.gitasupersite.iitk.ac.in/minigita/' + category + '?language=dv&field_chapter_value='+str(val0)+'&field_nsutra_value='+str(val1)
+   idy = str(val0) + '.' + str(val1)
+   pidy = str(val0) + '.' + str(val1 - 1)
+   nidy = str(val0) + '.' + str(val1 + 1)
+   while(True):
+       try:
+           html = urllib.urlopen(Url).read()
+           break
+       except Exception:
+           pass
+   html = html.replace('\r','')
+   soup = BeautifulSoup.BeautifulSoup(html)
+   FONT = soup.findAll('font', {'size':'3px'})
+   
+   san = ''
+   try:
+      x = FONT[0].findAll(text = True)
+   except IndexError:
+      return None
+   if(str(val1) in html):
+      pass
+   else:
+      return None
+   L = len(x)
+   for i in x:
+      first = str(i.encode('utf8'))
+      san += first + ' '
+   san = san.strip()
+   san = san.strip(' ')
+   san = san.strip('\n')
+   return san
+
+def get(chapter):
+   Mem = []
+   Fin = []
+   Count = 0
+   for i in xrange(1, 301):
+      print i,
+      sys.stdout.flush()
+      san = Parse(chapter, i)
+      if(san == None):
+         Count += 1
+         if(Count >= 20):
+            break
+      else:
+         Count = 0
+      if(san in Mem):
+         continue
+      else:
+         Mem.append(san)
+      if(len(Mem) >= 25):
+         Mem = Mem[1:]
+      if(san != None):
+         if(san in Fin):
+            break
+         else:
+            Fin.append(san)
+         Count = 0
+         f = open(category + '/' + str(chapter) + '_sanskrit.txt', 'a+')
+         f.write(san + '\n')
+         f.close()
+      else:
+         Count += 1
+         if(Count >= 20):
+            break
+
+#cat = [('ashtavakra', 20), ('avadhuta', 8), ('kapila', 3), ('sriram', 1), ('sruti', 2), ('uddhava', 24), ('vibhishana', 1)]
+cat = [('sriram', 1), ('sruti', 2), ('uddhava', 24), ('vibhishana', 1)]
+for i in cat:
+   category, chapter = i[0], i[1]
+   os.system('mkdir ' + category)
+   print category, ":"
+   for j in xrange(1, chapter + 1):
+      print "  ", j, ":",
+      get(j)
+      sys.stdout.flush()
+      print
+   print
